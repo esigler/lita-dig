07e97a0eb658012f0c04513023207c9eb1393694
Eric Sigler <me@esigler.com>
2014-12-04 13:26:19 -0800

    Get rid of crazy types case statement
    Finally get rid of that terrible type-picking case statement.  Much
    cleaner and future proof way of picking a record type now, and has the
    benefit of closing out most of the rest of the Rubocop complaints.

--- lib/lita/handlers/dig.rb
+++ lib/lita/handlers/dig.rb
@@ -1,6 +1,12 @@
 module Lita
   module Handlers
     class Dig < Handler
+      DNS_TYPES = %w(a ns md cname soa mb mg mr null wks ptr hinfo minfo
+                     mx txt rp afsdb x25 isdn rt nsap nsapptr sig key px
+                     gpos aaaa loc nxt eid nimloc srv atma naptr kx cert
+                     dname opt ds sshfp rrsig nsec dnskey uinfo uid gid
+                     unspec tkey tsig ixfr axfr mailb maila any)
+
       route(
         /^dig\s(\S+)(\s\+short)?$/,
         :resolve,
@@ -70,118 +76,8 @@ module Lita
       private
 
       def lookup(argument, type, server = nil)
-        case type
-        when 'a', 'A'
-          type = Net::DNS::A
-        when 'ns', 'NS'
-          type = Net::DNS::NS
-        when 'md', 'MD'
-          type = Net::DNS::MD
-        when 'cname', 'CNAME'
-          type = Net::DNS::CNAME
-        when 'soa', 'SOA'
-          type = Net::DNS::SOA
-        when 'mb', 'MB'
-          type = Net::DNS::MB
-        when 'mg', 'MG'
-          type = Net::DNS::MG
-        when 'mr', 'MR'
-          type = Net::DNS::MR
-        when 'null', 'NULL'
-          type = Net::DNS::NULL
-        when 'wks', 'WKS'
-          type = Net::DNS::WKS
-        when 'ptr', 'PTR'
-          type = Net::DNS::PTR
-        when 'hinfo', 'HINFO'
-          type = Net::DNS::HINFO
-        when 'minfo', 'MINFO'
-          type = Net::DNS::MINFO
-        when 'mx', 'MX'
-          type = Net::DNS::MX
-        when 'txt', 'TXT'
-          type = Net::DNS::TXT
-        when 'rp', 'RP'
-          type = Net::DNS::RP
-        when 'afsdb', 'AFSDB'
-          type = Net::DNS::AFSDB
-        when 'x25', 'X25'
-          type = Net::DNS::X25
-        when 'isdn', 'ISDN'
-          type = Net::DNS::ISDN
-        when 'rt', 'RT'
-          type = Net::DNS::RT
-        when 'nsap', 'NSAP'
-          type = Net::DNS::NSAP
-        when 'nsapptr', 'NSAPPTR'
-          type = Net::DNS::NSAPPTR
-        when 'sig', 'SIG'
-          type = Net::DNS::SIG
-        when 'key', 'KEY'
-          type = Net::DNS::KEY
-        when 'px', 'PX'
-          type = Net::DNS::PX
-        when 'gpos', 'GPOS'
-          type = Net::DNS::GPOS
-        when 'aaaa', 'AAAA'
-          type = Net::DNS::AAAA
-        when 'loc', 'LOC'
-          type = Net::DNS::LOC
-        when 'nxt', 'NXT'
-          type = Net::DNS::NXT
-        when 'eid', 'EID'
-          type = Net::DNS::EID
-        when 'nimloc', 'NIMLOC'
-          type = Net::DNS::NIMLOC
-        when 'srv', 'SRV'
-          type = Net::DNS::SRV
-        when 'atma', 'ATMA'
-          type = Net::DNS::ATMA
-        when 'naptr', 'NAPTR'
-          type = Net::DNS::NAPTR
-        when 'kx', 'KX'
-          type = Net::DNS::KX
-        when 'cert', 'CERT'
-          type = Net::DNS::CERT
-        when 'dname', 'DNAME'
-          type = Net::DNS::DNAME
-        when 'opt', 'OPT'
-          type = Net::DNS::OPT
-        when 'ds', 'DS'
-          type = Net::DNS::DS
-        when 'sshfp', 'SSHFP'
-          type = Net::DNS::SSHFP
-        when 'rrsig', 'RRSIG'
-          type = Net::DNS::RRSIG
-        when 'nsec', 'NSEC'
-          type = Net::DNS::NSEC
-        when 'dnskey', 'DNSKEY'
-          type = Net::DNS::DNSKEY
-        when 'uinfo', 'UINFO'
-          type = Net::DNS::UINFO
-        when 'uid', 'UID'
-          type = Net::DNS::UID
-        when 'gid', 'GID'
-          type = Net::DNS::GID
-        when 'unspec', 'UNSPEC'
-          type = Net::DNS::UNSPEC
-        when 'tkey', 'TKEY'
-          type = Net::DNS::TKEY
-        when 'tsig', 'TSIG'
-          type = Net::DNS::TSIG
-        when 'ixfr', 'IXFR'
-          type = Net::DNS::IXFR
-        when 'axfr', 'AXFR'
-          type = Net::DNS::AXFR
-        when 'mailb', 'MAILB'
-          type = Net::DNS::MAILB
-        when 'maila', 'MAILA'
-          type = Net::DNS::MAILA
-        when 'any', 'ANY'
-          type = Net::DNS::ANY
-        else
-          return t('error.unknown_type')
-        end
+        return t('error.unknown_type') unless DNS_TYPES.include?(type.downcase)
+        type = Object.const_get('Net::DNS::' + type.upcase)
 
         resolver = Net::DNS::Resolver.new
         resolver.nameservers = server unless server.nil?
