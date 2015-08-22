fd735840552a8c86adc329e7f0097c8de4fd8c82
Eric Sigler <me@esigler.com>
2015-08-22 12:03:15 -0700

    Add more RSpec fancyness

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,20 @@ require 'lita-dig'
 require 'lita/rspec'
 
 Lita.version_3_compatibility_mode = false
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.order = :random
+
+  Kernel.srand config.seed
+end
